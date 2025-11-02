120ad07b104fd956e1fc74fc006ff9cc233201cd
Stefan Muller <stmuller@c3r1p1.42vienna.com>
2025-11-02 21:38:56 +0100

    started + utils functions

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stmuller <stmuller@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 15:25:19 by stmuller          #+#    #+#             */
+/*   Updated: 2025/11/02 21:32:34 by stmuller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *string, int tosearch);
+size_t	ft_strlen(char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stmuller <stmuller@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/02 15:25:06 by stmuller          #+#    #+#             */
+/*   Updated: 2025/11/02 21:33:46 by stmuller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = 0;
+	return (str);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*r;
+
+	r = s;
+	i = 0;
+	while (i < n)
+	{
+		*r = 0;
+		r++;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*array;
+
+	if (nmemb == 0 || size == 0)
+	{
+		array = (malloc(1));
+		ft_bzero(array, 1);
+		return (array);
+	}
+	if (size >= SIZE_MAX / nmemb)
+		return (0);
+	array = malloc(size * nmemb);
+	if (!array)
+		return (0);
+	ft_bzero(array, nmemb * size);
+	return (array);
+}
+
+char	*ft_strchr(const char *string, int tosearch)
+{
+	char			*str;
+	unsigned char	c;
+
+	c = tosearch;
+	str = (char *)string;
+	while (*str != c && *str != 0)
+		str++;
+	if (*str == c)
+		return (str);
+	else
+		return (NULL);
+}
